5b6c7243d2622364ac20c42ad680d30fbb468d05
text <thesn515@gmail.com>
2021-04-18 10:47:37 +0900

    Second day edit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Route } from 'react-router';
+import AppRouter from 'components/Router';
+import {authService} from "fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggiedIn={isLoggedIn}/>;
+      <footer>&copy; Pwitter {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+export default App;
