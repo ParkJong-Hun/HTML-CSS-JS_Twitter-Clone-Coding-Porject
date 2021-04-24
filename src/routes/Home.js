588175820c3e990f9047e2b140ffec0f5f8be0b8
text <thesn515@gmail.com>
2021-04-24 11:06:12 +0900

    realtime database and get new database document and get user id

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,21 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({userObj}) => {
     const[pweet, setPweet] = useState("");
+    const [pweets, setPweets] = useState([]);
+    useEffect(() => {
+        dbService.collection("pweets").onSnapshot(snapshot => {
+            const pweetArray = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
+            setPweets(pweetArray);
+        })
+    }, []);
     const onSubmit =async (event) => {
         event.preventDefault();
         await dbService.collection("pweets").add({
-            pweet,
+            text:pweet,
             createdAt: Date.now(),
+            creatorId: userObj.uid
         });
         setPweet("");
     }
@@ -21,6 +29,13 @@ const Home = () => {
                 <input value = {pweet} onChange ={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Pweet"/>
             </form>
+            <div>
+                {pweets.map((pweet) => (
+                    <div key={pweet.id}>
+                        <h4>{pweet.text}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
